21848c7727a78a9db63f547c0f2d5a2f001ace76
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-11-13 14:15:41 +0100

    lite mer

--- Nasan-over-E-1/Program.cs
+++ Nasan-over-E-1/Program.cs
@@ -1,6 +1,51 @@
-﻿if (6 >= 3)
-{
-    Console.WriteLine("Hello World");
-}
+﻿// if (6 >= 3)
+//{
+//    Console.WriteLine("Hello World");
+//}
 
-Console.ReadLine();
+//Console.ReadLine();
+
+//Console.WriteLine("Type in username");
+//string name = Console.ReadLine();
+//if (name == "kalleanka");
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// Console.ReadLine();
+
+// Console.WriteLine("Type in username");
+// string name = Console.ReadLine();
+
+// if  (name == "noname")
+// {
+//     Console.WriteLine("Type in password");
+//     string pass = Console.ReadLine();
+//     if  (pass == "nopass")
+//     {
+//         Console.WriteLine("Welcome");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Try again");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("try again!");
+// }
+// Console.ReadLine();
+
+// for (int hello = 0; hello < 32; hello++)
+// {
+//     Console. WriteLine("Hello, world");
+// }
+// Console.ReadLine(); 
+
+// string lösenord = "";
+// while (lösenord != "Apelsin")
+// {
+//     Console.WriteLine("Testa igen");
+//     lösenord = Console.ReadLine();
+// } 
+// Console.ReadLine();
+
--- Nasan-over-E-1/obj/Debug/net8.0/Nasan-over-E-1.AssemblyInfo.cs
+++ Nasan-over-E-1/obj/Debug/net8.0/Nasan-over-E-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nasan-over-E-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77ba373d465268eb8e2bed9cbcbc045b908f8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nasan-over-E-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nasan-over-E-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
